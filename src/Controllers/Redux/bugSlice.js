de9602b34e8cdd0feb6bf2518e533a942c6aa454
lizdmukami <99487610+lizdmukami@users.noreply.github.com>
2023-05-13 00:28:51 +0000

    add bug slice controller

---
+++ src/Controllers/Redux/bugSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+    name:"bug",
+    initialState:{
+        getBugs:(state)=>{
+
+        },
+        createBugs:(state,actions)=>{
+
+        },
+        updateBugs:(state,actions)=>{
+
+        },
+        markComplete:(state,actions)=>{
+            
+        }
+    }
+})
